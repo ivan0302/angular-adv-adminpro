c3bf861a8f80615b69d9c21853fcb7c3b011984a
ivan0302 <iaarellano@hotmail.com>
2020-09-24 16:41:20 -0500

    Fin seccion 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,36 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './login/auth/login.component';
-import { RegisterComponent } from './login/register/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { PagesComponent } from './pages/pages.component';
+
+import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
+
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 const routes: Routes = [
-  { 
-    path: '',
-    component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path:'progress', component: ProgressComponent  }, 
-      { path:'grafica1', component: Grafica1Component},
-      { path:'', redirectTo: '/dashboard', pathMatch:'full' },
-    ]
-  },
-
-  { path:'register', component: RegisterComponent  },
-  { path:'login', component: LoginComponent  }, 
-
   
+  // path:'dashboard' PagesRouting
+  // path: 'auth' AuthRouting
+  { path:'', redirectTo:'/dashboard', pathMatch: 'full'},
   { path:'**', component: NopagefoundComponent},
 ];
 
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule
   ],
   exports: [RouterModule]
 })
